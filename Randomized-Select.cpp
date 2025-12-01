b88b3ce60bc7e1bfe8fb155d8d0ab826008c6dbf
rajpradeep02 <150119697+rajpradeep02@users.noreply.github.com>
2025-12-01 19:38:48 +0530

    Update Randomized-Select.cpp

--- Randomized-Select.cpp
+++ Randomized-Select.cpp
@@ -1,4 +1,4 @@
-// Write a program to find the ith smallest element of an array using Randomized Select.
+
 
 #include <iostream>
 #include <cstdlib>
@@ -6,16 +6,15 @@ using namespace std;
 
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1);     // Index of smaller element
+    int pivot = arr[high]; 
+    int i = (low - 1);     
 
     for (int j = low; j <= high - 1; j++)
     {
-        // If current element is smaller than or
-        // equal to pivot
+       
         if (arr[j] <= pivot)
         {
-            i++; // increment index of smaller element
+            i++; 
             swap(arr[i], arr[j]);
         }
     }
@@ -26,23 +25,23 @@ int partition(int arr[], int low, int high)
 int randomizedPartition(int arr[], int low, int high)
 {
     int pivotIndex = low + rand() % (high - low + 1);
-    swap(arr[pivotIndex], arr[high]); // Move pivot to end
+    swap(arr[pivotIndex], arr[high]); 
     return partition(arr, low, high);
 }
 
 int randomizedSelect(int arr[], int low, int high, int i)
 {
-    if (low == high) // If the list contains only one element
+    if (low == high)
         return arr[low];
 
     int partitionIndex = randomizedPartition(arr, low, high);
-    int k = partitionIndex - low + 1; // Number of elements in the left partition including pivot
+    int k = partitionIndex - low + 1; 
 
-    if (i == k) // The pivot value is the answer
+    if (i == k)
         return arr[partitionIndex];
-    else if (i < k) // Go to the left partition
+    else if (i < k) 
         return randomizedSelect(arr, low, partitionIndex - 1, i);
-    else // Go to the right partition
+    else 
         return randomizedSelect(arr, partitionIndex + 1, high, i - k);
 }
 
@@ -50,7 +49,7 @@ int main()
 {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int i = 3; // Find the 3rd smallest element
+    int i = 3; 
     cout << "The " << i << "th smallest element is " << randomizedSelect(arr, 0, n - 1, i) << endl;
     return 0;
 }
